e1eca609b2c9ebc05274a56ca7530d8c7a26d574
Ben Leitl <ben.leitl@me.com>
2020-03-09 08:47:30 +0100

    Add functionality

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,26 @@
 package com.codersbay;
 
+import java.util.Scanner;
+
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter an int value. You will receive the Fibonacci of your input:");
+        int input = scanner.nextInt();
+
+        System.out.println("Your Fibonacci number is:");
+        System.out.println(fibonacci(10));
+
+    }
+    public static int fibonacci(int n) {
+        if (n <2) {
+            return n;
+        }
+        else {
+            return (fibonacci(n-1) + fibonacci(n-2));
+        }
+    }
 }
